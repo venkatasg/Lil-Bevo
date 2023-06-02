aef4d8bb40d36c427f73d55eedb0af11fe8d4afc
Kaj Bostrom <kaj@cs.utexas.edu>
2023-06-02 12:45:57 -0700

    add tokenizer training wrapper script

---
+++ tokenizers/train_tokenizer.py
@@ -0,0 +1,20 @@
+import sentencepiece as spm
+import argparse
+
+if __name__ == "__main__":
+    argp = argparse.ArgumentParser()
+    argp.add_argument('input_files', type=str, nargs='+')
+    argp.add_argument('-k', '--vocab-size', type=int, required=True)
+    argp.add_argument('-o', '--output-prefix', type=str, required=True)
+    argp.add_argument('--split-digits', action='store_true')
+    argp.add_argument('--byte-fallback', action='store_true')
+    args = argp.parse_args()
+
+    spm.SentencePieceTrainer.train(
+        input=','.join(args.input_files),
+        model_type='unigram',
+        model_prefix=args.output_prefix,
+        vocab_size=args.vocab_size,
+        split_digits=args.split_digits,
+        byte_fallback=args.byte_fallback
+    )
