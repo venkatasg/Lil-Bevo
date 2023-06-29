34a2276666a7e866a57c41a814fe212c43a2d346
Venkat <gvenkata1994@gmail.com>
2023-06-29 17:54:20 -0500

    Fast tokenizer with mask, cls, padding

--- tokenizers/convert_to_hf_tokenizer.py
+++ tokenizers/convert_to_hf_tokenizer.py
@@ -1,4 +1,4 @@
-from transformers import T5Tokenizer
+from transformers import T5TokenizerFast
 import argparse
 
 if __name__ == "__main__":
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     argp.add_argument('--output_dir', type=str, required=True)
     args = argp.parse_args()
     
-    tokenizer = T5Tokenizer(args.tokenizer_file)
+    tokenizer = T5TokenizerFast(vocab_file=args.tokenizer_file, extra_ids=0)
     tokenizer.mask_token = '<mask>'
     tokenizer.cls_token = '<cls>'
+    tokenizer.model_max_length = 512
     tokenizer.save_pretrained(args.output_dir)
